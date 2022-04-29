01827d485153600393a2f982109ce6adc895223d
Kuro-Rui <louisdominic80@gmail.com>
2022-04-29 20:20:20 +0700

    I'm always this dumb

--- dislashcontext/__init__.py
+++ dislashcontext/__init__.py
@@ -10,23 +10,18 @@ class DislashContext(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, 9937656857, True)
-        if self.saved_config:
-            default_global = self.saved_config
-        else:
-            default_global = {"send_monkeypatch": None}
+        default_global = {"send_monkeypatch": None}
         self.config.register_global(**default_global)
-        self.settings = {}
 
     async def initialize(self):
-        self.settings = await self.config.all()
-        self.config.register_global(**self.settings)
-        if self.settings["send_monkeypatch"]:
-            setattr(commands.Context, self.settings["send_monkeypatch"], send_with_components)
+        send_monkeypatch = await self.config.send_monkeypatch()
+        if send_monkeypatch and not hasattr(commands.Context, send_monkeypatch):
+            setattr(commands.Context, send_monkeypatch, send_with_components)
 
     def cog_unload(self):
-        if self.settings["send_monkeypatch"] and hasattr(commands.Context, self.settings["send_monkeypatch"]):
-            delattr(commands.Context, self.settings["send_monkeypatch"])
-        self.saved_config = self.settings
+        send_monkeypatch = await self.config.send_monkeypatch()
+        if send_monkeypatch and hasattr(commands.Context, send_monkeypatch):
+            delattr(commands.Context, send_monkeypatch)
 
     @commands.is_owner()
     @commands.group()
@@ -45,10 +40,11 @@ class DislashContext(commands.Cog):
         """
 
         if name:
-            if self.settings["send_monkeypatch"]:
-                delattr(commands.Context, self.settings["send_monkeypatch"])
+            send_monkeypatch = await self.config.send_monkeypatch()
+            if send_monkeypatch:
+                delattr(commands.Context, send_monkeypatch)
             setattr(commands.Context, name, send_with_components)
-            self.settings["send_monkeypatch"] = name
+            await self.config.send_monkeypatch.set(name)
             await ctx.tick()
             code = box(
                 (
@@ -67,20 +63,19 @@ class DislashContext(commands.Cog):
                 (
                     "The monkeypatched send has been set to `ctx.{send}`. "
                     "To test it, run:\n{prefix}eval {code}"
-                ).format(send=self.settings["send_monkeypatch"], prefix=ctx.prefix, code=code)
+                ).format(send=send_monkeypatch, prefix=ctx.prefix, code=code)
             )
 
     @dctx.command(name="clear", aliases=["remove", "rem"])
     async def dctx_clear(self, ctx: commands.Context):
         """Removes the current monkeypatched `ctx.send`"""
 
-        if not self.settings["send_monkeypatch"]:
+        send_monkeypatch = await self.config.send_monkeypatch()
+        if not send_monkeypatch:
             return await ctx.send("Bruh you didn't even set any before.")
-        monkeypatched_send = self.settings["send_monkeypatch"]
-        delattr(commands.Context, monkeypatched_send)
+        delattr(commands.Context, send_monkeypatch)
         await ctx.tick()
-        await ctx.send(f"The `ctx.{monkeypatched_send}` has been removed.")
-        self.settings["send_monkeypatch"] = None
+        await ctx.send(f"The `ctx.{send_monkeypatch}` has been removed.")
 
 
 async def setup(bot):
