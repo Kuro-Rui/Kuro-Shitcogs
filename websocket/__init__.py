05d3ef4e0bb302a0346749fe0dd82d328dfb5404
Louis Dominic <83048873+Kuro-Rui@users.noreply.github.com>
2023-07-05 20:59:14 +0700

    Update __init__.py

--- websocket/__init__.py
+++ websocket/__init__.py
@@ -1,9 +1,7 @@
-import asyncio
 from discord.gateway import DiscordWebSocket
 from redbot.core import Config, commands
 
 from .converters import WSConverter
-from .websockets import *
 
 
 class WebSocket(commands.Cog):
@@ -13,16 +11,31 @@ class WebSocket(commands.Cog):
         self.config = Config.get_conf(self, 91398293891669, True)
         self.config.register_global(websocket="Web")
 
-    def cog_unload(self):
-        DiscordWebSocket.identify = Web.identify
+        self.old_identify = DiscordWebSocket.identify
+
+    async def cog_load(self):
+        websocket = await self.config.websocket()
+        if websocket == "Web":
+            return
+        if websocket == "Android":
+            DiscordWebSocket.identify = Android.identify
+        elif websocket == "iOS":
+            DiscordWebSocket.identify = iOS.identify
+        elif websocket == "Client":
+            DiscordWebSocket.identify = Client.identify
+        for shard_id, shard in bot.shards.items():
+            await bot.shards[shard_id].reconnect()
+
+    async def cog_unload(self):
+        DiscordWebSocket.identify = self.old_identify
         for shard_id, shard in self.bot.shards.items():
-            asyncio.create_task(self.bot.shards[shard_id].reconnect())
+            await self.bot.shards[shard_id].reconnect()
 
     @commands.is_owner()
     @commands.command()
     async def websocket(self, ctx, websocket: WSConverter):
         """
-        Bot websocket monkeypatch? (No irdumb)
+        Change websocket ig.
 
         You can choose from `Android`, `iOS`, `Client`, or `Web`.
         """
@@ -36,8 +49,7 @@ class WebSocket(commands.Cog):
             await self.config.websocket.set("Client")
             DiscordWebSocket.identify = Client.identify
         elif websocket == "web":
-            await self.config.websocket.set("Web")
-            DiscordWebSocket.identify = Web.identify
+            DiscordWebSocket.identify = self.old_identify
         await ctx.send("Ok, shards will restart for a short time.")
         for shard in self.bot.shards.values():
             await self.bot.shards[shard].reconnect()
@@ -45,16 +57,4 @@ class WebSocket(commands.Cog):
 
 
 async def setup(bot):
-    bot.add_cog(WebSocket(bot))
-
-    websocket = await bot.get_cog("WebSocket").config.websocket()
-    if websocket == "Web":
-        return
-    if websocket == "Android":
-        DiscordWebSocket.identify = Android.identify
-    elif websocket == "iOS":
-        DiscordWebSocket.identify = iOS.identify
-    elif websocket == "Client":
-        DiscordWebSocket.identify = Client.identify
-    for shard_id, shard in bot.shards.items():
-        await bot.shards[shard_id].reconnect()
+    await bot.add_cog(WebSocket(bot))
